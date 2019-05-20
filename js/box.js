3850fc46bf0f8f920c7077e187dd75be0b05aea2
jinhuachen1314 <jic212@ucsd.edu>
2019-05-19 21:58:18 -0700

    add basic logic

--- js/box.js
+++ js/box.js
@@ -1,5 +1,10 @@
 class Box {
-  constructor(c) {
-    
+  constructor(c, x, y, text) {
+    this.c = c;
+    this.x = x;
+    this.y = y;
+    this.text = text;
   }
 }
+
+export default Box;
--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+import Box from "./box";
+
 class Game {
   constructor(c) {
     // initialize board, boxes, speed, music
@@ -6,12 +8,21 @@ class Game {
     this.audio = document.getElementById("audio");
     this.music = document.getElementById("music");
     this.input = document.getElementById("input");
+
+    // when game starts
+    this.spawnY = 25;
+    this.spawnRate = 1500;
+    this.spawnRateOfDescent = 0.50;
+    this.lastSpawn = -1;
+    this.boxes = [];
+    this.startTime = Date.now();
     this.audio.load();
+    this.animate = this.animate.bind(this);
+    this.spawnRandomObject = this.spawnRandomObject.bind(this);
   } 
 
   // function to start the game
   playGame() {
-
     this.music.style.display = "flex";
     this.input.style.display = "flex";
     this.music.addEventListener("click", () => {
@@ -28,7 +39,69 @@ class Game {
     })
 
     this.audio.play();
+    this.animate();
+   }
+
+  animate() {
+    this.input.addEventListener("input", e => {
+      // console.log(e.target.value);
+      if (e.target.value === this.boxes[0].text) {
+        // console.log("cancel");
+        this.boxes.shift();
+        e.target.value = "";
+      }
+    })
+
+    const time = Date.now();
+  
+    if (time > (this.lastSpawn + this.spawnRate)) {
+      this.lastSpawn = time;
+      this.spawnRandomObject();
+    }
+    
+    requestAnimationFrame(this.animate);
+    
+    this.c.clearRect(0, 0, this.c.canvas.width, this.c.canvas.height);
+    
+    this.c.beginPath();
+    this.c.moveTo(0, this.spawnY);
+    this.c.lineTo(this.c.canvas.width, this.spawnY);
+    this.c.stroke();
+
+    for (let i = 0; i < this.boxes.length; i++) {
+      const box = this.boxes[i];
+      box.y += this.spawnRateOfDescent;
+      this.c.beginPath();
+      // this.c.fillRect(box.x, box.y, 100, 30);
+      this.c.fillStyle = "#000000";
+      this.c.fillText(box.text, box.x + 5, box.y + 1);
+      this.c.font = "30px Verdana"
+      this.c.closePath();
+      // this.c.fill();
+    }
   }
+
+  randomString() {
+    let result = "";
+    const characters = "abcdefghijklmnopqrstuvwxyz";
+    const length = Math.floor(Math.random() * 10);
+    const wordLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * wordLength));
+    }
+    return result;
+  }
+
+
+  spawnRandomObject() {
+    const box = new Box(this.c, 
+      Math.random() * (this.c.canvas.width - 30),
+      this.spawnY,
+      this.randomString());
+
+    this.boxes.push(box);
+  }
+
 }
 
 export default Game;
